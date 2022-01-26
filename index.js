28c687dbc36faef17c11cc9a7d2592302547b2fe
Matis Rammi <matis.rammi@khk.ee>
2022-01-26 14:59:25 +0200

    templating directory setup

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const path = require('path')
 const express = require('express')
 
 const app = express();
 
 app.use(express.static('public'));
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
 
 app.get('/user/:username', (req, res)=> {
     let user = req.params.username;
