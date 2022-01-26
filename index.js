4f3e95d5d782d0e7955c1340fceaae6d04262a9d
Matis Rammi <matis.rammi@khk.ee>
2022-01-26 12:28:39 +0200

    Basic express application

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.send('<a href="/contact">Contact us</a> <br> <a href="/about">About us</a>');
+});
+
+app.get('/contact', (req, res)=> {
+    res.send('<h1>Contact Us Page</h1>');
+})
+
+app.get('/about', (req, res)=> {
+    res.send('<h1>About Us Page</h1>');
+})
+
+app.get('*', (req, res)=> {
+    res.send('404. This page does not exist. <a href="/">Go to HomePage</a>')
+})
+
+app.listen(3000, ()=> {
+    console.log('Server started on http://localhost:3000');
+});
