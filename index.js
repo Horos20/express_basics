4dd9f1edc5cb2c4674361bdc54f3ca9ac56a5a21
Matis Rammi <matis.rammi@khk.ee>
2022-01-26 12:38:27 +0200

    Basic use of ejs

--- index.js
+++ index.js
@@ -2,22 +2,12 @@ const express = require('express')
 
 const app = express();
 
-app.get('/', (req, res)=> {
-    res.send('<a href="/contact">Contact us</a> <br> <a href="/about">About us</a>');
+app.get('/user/:username', (req, res)=> {
+    let user = req.params.username;
+    res.render('index.ejs', {username: user});
 });
-
-app.get('/contact', (req, res)=> {
-    res.send('<h1>Contact Us Page</h1>');
-})
-
-app.get('/about', (req, res)=> {
-    res.send('<h1>About Us Page</h1>');
-})
-
-app.get('*', (req, res)=> {
-    res.send('404. This page does not exist. <a href="/">Go to HomePage</a>')
-})
 
 app.listen(3000, ()=> {
-    console.log('Server started on http://localhost:3000');
+    console.log('Server started on http://localhost:3000')
 });
+
